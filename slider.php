41382d1c906a20de9e0fee36c5718fda3af752ba
pooja <pooja@wpgenius.in>
2019-08-20 17:05:23 +0530

    Fix:slider taxonomy added which resloves issue #4

--- slider.php
+++ slider.php
@@ -1,10 +1,10 @@
 <?php
  /* Plugin Name:Post Slider
-    Description: Simple Plugin to display post slider
-    Plugin URI:http://pooja.tyche.work
-    version:1.0
-    Author: Team Tyache Ventures
-    Author URI:http:/pooja.tyche.work
+   * Description: Simple Plugin to display post slider
+   * Plugin URI:http://pooja.tyche.work
+   *  version:1.0
+   * Author: Team Tyache Ventures
+   * Author URI:http:/pooja.tyche.work
 */
 
 defined( 'ABSPATH' ) or die( 'No script kiddies please!' );
